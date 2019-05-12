42a9a0fcfa2bd52e99e510e6b29734b7daaa5eea
stayi <vardeath@yandex.ru>
2019-05-13 01:03:17 +0300

    new test project

--- app/src/main/java/com/android/malsoftware/techadvisor/BaseItemViewModel.java
+++ app/src/main/java/com/android/malsoftware/techadvisor/BaseItemViewModel.java
@@ -2,19 +2,30 @@ package com.android.malsoftware.techadvisor;
 
 import androidx.databinding.BaseObservable;
 import androidx.databinding.Bindable;
+import androidx.databinding.ObservableField;
+import androidx.databinding.ObservableInt;
+
 
 public class BaseItemViewModel extends BaseObservable {
 
+	private ObservableField<String> mText = new ObservableField<>();
+	private ObservableInt mBackground = new ObservableInt();
 
-	public String getText() {
+	public ObservableField<String> getText() {
 		return mText;
 	}
 
 	@Bindable
-	public void setText(String text) {
-		mText = text;
-		notifyChange();
+	void setText(String text) {
+		mText.set(text);
 	}
 
-	private String mText = "Text of Rabbit";
+	public int getBackground() {
+		return mBackground.get();
+	}
+
+	@Bindable
+	void setBackground(int background) {
+		mBackground.set(background);
+	}
 }
--- app/src/main/java/com/android/malsoftware/techadvisor/MainMenuFragment.java
+++ app/src/main/java/com/android/malsoftware/techadvisor/MainMenuFragment.java
@@ -1,29 +1,40 @@
 package com.android.malsoftware.techadvisor;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.android.malsoftware.techadvisor.databinding.MillItemSimpleBinding;
+import com.android.malsoftware.techadvisor.databinding.MillItemDefaultBinding;
+import com.android.malsoftware.techadvisor.databinding.MillItemSelectedBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class MainMenuFragment extends Fragment {
 
-	private int pos = 0;
-	private int mCount = 0;
-	private MainMenuAdaptor mMainMenuAdaptor;
+	private List<String> mItemsArray = new ArrayList<>();
+	private RecyclerView mRecyclerView;
+	private MainMenuAdaptor mainMenuAdaptor;
+	private View rootView;
+	private int mItemQuantity;
+	private int mViewHeight;
+	private int mItemHeight;
+	private boolean isGlobalLayoutInflated = false;
+	private int mCurrentSelectedPosition = 0;
 
 	static MainMenuFragment newInstance() {
 		return new MainMenuFragment();
@@ -32,72 +43,147 @@ public class MainMenuFragment extends Fragment {
 	@Override
 	public void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		for (int i = 0; i < 17; ++i) {
+			mItemsArray.add("item" + i);
+		}
 	}
 
 	@Nullable
 	@Override
 	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.menu_main_fragment, container, false);
+		rootView = inflater.inflate(R.layout.menu_main_fragment, container, false);
 
-		final RecyclerView recyclerView = rootView.findViewById(R.id.main_resycleview);
-		recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 1));
-		mMainMenuAdaptor = new MainMenuAdaptor();
-		recyclerView.setAdapter(mMainMenuAdaptor);
-
-
+		mRecyclerView = rootView.findViewById(R.id.main_resycleview);
+		RecyclerView.LayoutManager mLayManager = new LinearLayoutManager(getActivity());
+		mRecyclerView.setLayoutManager(mLayManager);
 		Button btn = rootView.findViewById(R.id.button);
+		getViewSize(mRecyclerView);
+
+		updateUi();
+
 		btn.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Log.d("resview", "clicked_button");
-				recyclerView.setNestedScrollingEnabled(true);
-				recyclerView.scrollToPosition(pos);
-				recyclerView.performClick();
-				if (pos < 20) ++pos;
-				else pos = 0;
+				if (isGlobalLayoutInflated) {
+					int count = mRecyclerView.getScrollState();
+					//mRecyclerView.smoothScrollToPosition(mItemQuantity + (mItemQuantity - 2));
+
+					Log.d("mar2", "itemQuantity = " + mItemQuantity);
+					Log.d("mar2", "itemHeight = " + mItemHeight);
+					Log.d("mar2", "viewHeight = " + mViewHeight);
+					Log.d("mar2", "count = " + count);
+				}
 			}
 		});
 
 		return rootView;
 	}
 
-	private class MainMenuHolder extends RecyclerView.ViewHolder {
+	private class MainMenuHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-		private MillItemSimpleBinding mMillItemSimpleBinding;
+		private MillItemDefaultBinding mMillItemDefaultBinding;
+		private MillItemSelectedBinding mMillItemSelectedBinding;
 
-		MainMenuHolder(MillItemSimpleBinding illItemSimpleBinding) {
-			super(illItemSimpleBinding.getRoot());
-			mMillItemSimpleBinding = illItemSimpleBinding;
-			mMillItemSimpleBinding.setBaseModel(new BaseItemViewModel());
+		MainMenuHolder(MillItemDefaultBinding millItemDefaultBinding) {
+			super(millItemDefaultBinding.getRoot());
+			mMillItemDefaultBinding = millItemDefaultBinding;
+			mMillItemDefaultBinding.setBaseModel(new BaseItemViewModel());
+			itemView.setOnClickListener(this);
 		}
 
-		@SuppressLint("SetTextI18n")
-		void bind(int position) {
-			mMillItemSimpleBinding.getBaseModel().setText("Item: " + mCount);
-			if (mCount < 20) ++mCount;
-			//else mCount = 0;
+		MainMenuHolder(MillItemSelectedBinding millItemSelectedBinding) {
+			super(millItemSelectedBinding.getRoot());
+			mMillItemSelectedBinding = millItemSelectedBinding;
+			mMillItemSelectedBinding.setBaseModel(new BaseItemViewModel());
+			itemView.setOnClickListener(this);
+		}
+
+		void bind(int position, String val, int itemViewType) {
+			if (itemViewType == 1) mMillItemDefaultBinding.getBaseModel().setText(val + position);
+			if (itemViewType == 2) mMillItemSelectedBinding.getBaseModel().setText(val + position);
+		}
+
+		@Override
+		public void onClick(View v) {
+			int oldPos = mCurrentSelectedPosition;
+			mCurrentSelectedPosition = getLayoutPosition();
+			mainMenuAdaptor.notifyItemChanged(mCurrentSelectedPosition);
+			mainMenuAdaptor.notifyItemChanged(oldPos);
+			Log.d("mar2", "mCurrentSelectedPosition" + mCurrentSelectedPosition);
+			Log.d("mar2", "oldPos" + oldPos);
 		}
 	}
 
 	private class MainMenuAdaptor extends RecyclerView.Adapter<MainMenuHolder> {
+
+		private List<String> Array;
+		private MillItemDefaultBinding mMillItemDefaultBinding;
+		private MillItemSelectedBinding mMillItemSelectedBinding;
+
+		MainMenuAdaptor(List<String> Array) {
+			this.Array = Array;
+		}
+
+		@Override
+		public int getItemViewType(int position) {
+			super.getItemViewType(position);
+			if (position == mCurrentSelectedPosition) {
+				return 2;
+			} else return 1;
+		}
 
 		@NonNull
 		@Override
 		public MainMenuHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
 			LayoutInflater inflater = LayoutInflater.from(getActivity());
-			MillItemSimpleBinding mMillItemSimpleBinding = DataBindingUtil.inflate(inflater,R.layout.mill_item_simple, parent, false);
-			return new MainMenuHolder(mMillItemSimpleBinding);
+			if (viewType == 1) {
+				mMillItemDefaultBinding = DataBindingUtil.inflate(inflater, R.layout.mill_item_default, parent, false);
+				return new MainMenuHolder(mMillItemDefaultBinding);
+			}
+			else {
+				mMillItemSelectedBinding = DataBindingUtil.inflate(inflater, R.layout.mill_item_selected, parent, false);
+			return new MainMenuHolder(mMillItemSelectedBinding);
+			}
 		}
 
 		@Override
 		public void onBindViewHolder(@NonNull MainMenuHolder holder, int position) {
-			holder.bind(position);
+			holder.bind(position, Array.get(position), getItemViewType(position));
 		}
 
 		@Override
 		public int getItemCount() {
-			return 20;
+			return mItemsArray.size();
+		}
+	}
+
+	private void updateUi() {
+		mainMenuAdaptor = new MainMenuAdaptor(mItemsArray);
+		mRecyclerView.setAdapter(mainMenuAdaptor);
+		Objects.requireNonNull(mRecyclerView.getItemAnimator()).setChangeDuration(0);
+	}
+
+	private void getViewSize(final View view) {
+		ViewTreeObserver viewTreeObserver = view.getViewTreeObserver();
+		if (viewTreeObserver.isAlive()) {
+			viewTreeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+				@Override
+				public void onGlobalLayout() {
+
+					FrameLayout linearLayout = rootView.findViewById(R.id.framelay1);
+					view.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+
+					final int viewHeight = view.getHeight();
+					final int itemHeight = (int) (linearLayout.getMeasuredHeight() + getResources().getDimension(R.dimen.offcet));
+					mItemQuantity = (int) Math.floor( (double) viewHeight / (double) itemHeight);
+					mViewHeight = viewHeight;
+					mItemHeight = itemHeight;
+
+					isGlobalLayoutInflated = true;
+				}
+			});
 		}
 	}
 }
