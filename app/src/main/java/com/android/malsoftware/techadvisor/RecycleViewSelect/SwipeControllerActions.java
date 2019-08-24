55b43c7005741b4521e7756bbf36c5503c5a1a5f
vardeath <vardeath@yandex.ru>
2019-08-24 11:00:17 +0300

    добавлен swipe

---
+++ app/src/main/java/com/android/malsoftware/techadvisor/RecycleViewSelect/SwipeControllerActions.java
@@ -0,0 +1,9 @@
+package com.android.malsoftware.techadvisor.RecycleViewSelect;
+
+public class SwipeControllerActions {
+
+    public void onLeftClicked(int position) {}
+
+    public void onRightClicked(int position) {}
+
+}
